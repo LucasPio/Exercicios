3d178b3672b2f3be9f31bb6e78f7bf984bdf0b25
LucasPio <96150523+LucasPio@users.noreply.github.com>
2021-12-15 07:54:08 -0300

    Add files via upload

---
+++ sorteador.py
@@ -0,0 +1,10 @@
+import random
+
+a=random.randint(0,  10)
+b= int(input("escolha um numero entre 0 e 10: "))
+
+if b == a:
+    print("você acertou, o numero era", a)
+
+else:
+    print("não foi dessa vez, o numero era", a)
