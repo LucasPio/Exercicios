ba5660843914c0ff769eda0d9da314a3fe89bba9
LucasPio <96150523+LucasPio@users.noreply.github.com>
2021-12-15 15:14:02 -0300

    Add files via upload

---
+++ chute o valor.py
@@ -0,0 +1,21 @@
+import random
+a = random.randint(0, 100)
+
+count = -1
+while count <= 0:
+
+
+
+    b = int(input("escolha um numero entre 0 e 10: "))
+
+
+    if b > a:
+            print("chute um valor menor")
+
+    elif b < a:
+            print("chute um valor maior")
+
+
+    else:
+            print("parabens você acertou", a)
+            count = b == a
